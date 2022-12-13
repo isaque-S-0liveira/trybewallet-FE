e5f4143004fb47064f5300593d04f4c92a122475
Isaque <zazac3179@gmail.com>
2022-12-13 15:26:32 -0300

    ajeitando req 4 pq deu erro no req 2

--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,6 +1,7 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
 const INITIAL_STATE_WALLET = {
   currencies: [], // array de string
+  valorTotal: 0,
   expenses: [], // array de objetos, com cada objeto tendo as chaves id, value, currency, method, tag, description e exchangeRates
   editor: false, // valor booleano que indica de uma despesa está sendo editada
   idToEdit: 0, // valor numérico que armazena o id da despesa que esta sendo editada
